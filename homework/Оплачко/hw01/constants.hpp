f86b47932f577e2f2435ffd04c3ffbc5cd062e66
magnickolas@gmail.com <magnickolas@gmail.com>
2017-09-22 19:50:58 +0300

    HW01
    corrects taking into advices

--- homework/Оплачко/hw01/constants.hpp
+++ homework/Оплачко/hw01/constants.hpp
@@ -2,6 +2,6 @@
 
 #include <cstddef>
 
-const std::size_t DIM_SIZE = 15000;
+const std::size_t DIM_SIZE = 10000;
 const unsigned int CYCLES_NUM = 20;
 const unsigned int HEATING_CYCLES_NUM = 5;
--- homework/Оплачко/hw01/timer.hpp
+++ homework/Оплачко/hw01/timer.hpp
@@ -4,28 +4,32 @@
 #include <iomanip>
 #include <chrono>
 
+namespace _Timer {
+	std::chrono::microseconds::rep durationsSum = 0;
+	unsigned int repeatsNumber = 0;
+
+	void updateAndPrintTimerStats(std::chrono::microseconds::rep duration) {
+		++repeatsNumber;
+		durationsSum += duration;
+		std::cout << "Repeat #" << repeatsNumber << ": " << duration << " us" <<
+			std::endl;
+	}
+	void printAverageTimerStats() {
+		std::cout << "Average time: " << std::fixed << std::setprecision(0) <<
+			static_cast<double>(durationsSum) / repeatsNumber << " us" <<
+			std::endl;
+	}
+}
+
 class Timer {
 public:
-	static std::chrono::microseconds::rep durationsSum;
-	static unsigned int repeatsNumber;
 	Timer() : start_(std::chrono::high_resolution_clock::now()) {}
 	~Timer() {
 		const auto finish = std::chrono::high_resolution_clock::now();
 		const auto duration = std::chrono::duration_cast<
 			std::chrono::microseconds>(finish - start_).count();
-		durationsSum += duration;
-		++repeatsNumber;
-		std::cout << "Repeat #" << repeatsNumber << ": " << duration << " us" <<
-			std::endl;
-	}
-	static void printAverageTime() {
-		std::cout << "Average time: " << std::fixed << std::setprecision(0) <<
-			static_cast<double>(durationsSum) / repeatsNumber << " us" <<
-			std::endl;
+		_Timer::updateAndPrintTimerStats(duration);
 	}
 private:
 	const std::chrono::high_resolution_clock::time_point start_;
 };
-
-std::chrono::microseconds::rep Timer::durationsSum = 0;
-unsigned int Timer::repeatsNumber = 0;
