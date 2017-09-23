e83d8f6ddd9478350b3a2a9e48a958a16afe4ed8
AlexanderIB31 <aleks_bales@mail.ru>
2017-09-23 16:10:04 +0300

    msu_work_01

---
+++ bales/01/slow.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+// #include <random>
+#include <chrono>
+
+static const size_t sz = 10000;
+
+class Timer {
+public:
+    Timer()
+        : start_(std::chrono::high_resolution_clock::now())
+    {
+    }
+
+    ~Timer()
+    {
+        const auto finish = std::chrono::high_resolution_clock::now();
+        std::cout << std::chrono::duration_cast<std::chrono::microseconds>(finish - start_).count() << " us" << std::endl;
+    }
+
+private:
+    const std::chrono::high_resolution_clock::time_point start_;
+};
+
+// void print(const int* arr) {
+//     for (size_t i = 0; i < sz; ++i) {
+//         for (size_t j = 0; j < sz; ++j) {
+//             std::cout << arr[i * sz + j] << " ";
+//         }
+//         std::cout << std::endl;
+//     }
+//     std::cout << "***************" << std::endl;
+// }
+
+int main(int argc, char** argv) { 
+    // std::random_device device;
+    // std::minstd_rand gen(device());
+    // std::uniform_int_distribution<int> distrib(-1000, 1000);
+
+    Timer t;
+    using TMatrix = int[sz * sz];
+    int* matrix = new TMatrix;
+
+    for (size_t i = 0; i < sz; ++i) {
+        for (size_t k = 0; k < sz; ++k) {
+            matrix[k * sz + i] = k * sz + i; 
+        }
+    }
+
+    for (size_t i = 0; i < sz; ++i) {
+        for (size_t k = 0; k < sz; ++k) {
+            matrix[k * sz + i] += matrix[k * sz + i]; 
+        }
+    }
+    // print(matrix);
+    delete[] matrix;
+    return 0;
+}
