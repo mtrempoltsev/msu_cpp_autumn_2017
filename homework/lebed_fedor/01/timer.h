85c52c01f71144e55bba4046d85d072aa26cf393
Fedor <fedorlebed.cs@gmail.com>
2017-09-23 20:31:40 +0300

    homework 01 done

---
+++ homework/lebed_fedor/01/timer.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <chrono>
+#include <iostream>
+
+class Timer
+{
+public:
+    Timer()
+        : start_(std::chrono::high_resolution_clock::now())
+    {
+    }
+
+    int elapsed()
+    {
+        const auto finish = std::chrono::high_resolution_clock::now();
+        return std::chrono::duration_cast<std::chrono::microseconds>(finish - start_).count();
+    }
+
+private:
+    const std::chrono::high_resolution_clock::time_point start_;
+};
