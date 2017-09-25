06205f1c265706e5a21eecd29c24156c88d99cbe
borisman18 <32166232+borisman18@users.noreply.github.com>
2017-09-25 21:15:04 +0300

    Add files via upload

---
+++ homework/Feldman/01/sum_by_columns.cpp
@@ -0,0 +1,50 @@
+#include <chrono>
+#include <iostream>
+using namespace std;
+#define N 10000
+class Timer
+{
+public:
+	Timer()
+		: start_(std::chrono::high_resolution_clock::now())
+	{
+	}
+
+	~Timer()
+	{
+		const auto finish = std::chrono::high_resolution_clock::now();
+		std::cout << std::chrono::duration_cast<std::chrono::microseconds>(finish - start_).count() << " us" << std::endl;
+	}
+
+private:
+	const std::chrono::high_resolution_clock::time_point start_;
+};
+
+int main() {
+	int** firstMatrix = new int*[N]; 
+
+	for (int i = 0; i < N; ++i) {
+		firstMatrix[i] = new int[N];
+		
+		for (int j = 0; j < N; ++j) { 
+			firstMatrix[i][j] = 1;
+			
+		}
+	}
+	int sum = 0;
+	{
+		Timer t;
+		for (int i = 0; i < N; ++i) {
+			for (int j = 0; j < N; ++j) {				
+				sum += firstMatrix[j][i]; 
+			}
+		}
+	}
+	for (int i = 0; i < N; ++i) {
+		delete[] firstMatrix[i];
+		
+	}
+	delete[] firstMatrix;
+	cout << sum << endl;
+	return 0;
+}
