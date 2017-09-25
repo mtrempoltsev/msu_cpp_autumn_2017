de4eb039616280d4c29570915bcd511119e63a3c
Maksim <maxapple@yandex.ru>
2017-09-26 00:13:02 +0300

    task1 files

---
+++ homework/Filin/01/task1_rows.cpp
@@ -0,0 +1,72 @@
+//
+//  main.cpp
+//  TS
+//
+//  Created by Максим on 21.09.17.
+//  Copyright © 2017 Максим. All rights reserved.
+//
+
+#include <iostream>
+#include <cstdio>
+#include <chrono>
+#include <random>
+
+
+#define M 10000
+#define N 10000
+#define MIN -100
+#define MAX 100
+
+using namespace std;
+
+
+class Timer
+{
+public:
+    Timer()
+    : start_(std::chrono::high_resolution_clock::now())
+    {
+    }
+    
+    ~Timer()
+    {
+        const auto finish = std::chrono::high_resolution_clock::now();
+        std::cout << std::chrono::duration_cast<std::chrono::microseconds>(finish - start_).count() << " us" << std::endl;
+    }
+    
+private:
+    const std::chrono::high_resolution_clock::time_point start_;
+};
+
+
+long long int sum_rows(int** matrix, int m, int n) {
+    volatile long long int sum = 0;
+    Timer t;
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            sum += matrix[i][j];
+        }
+    }
+    return sum;
+}
+
+int main(int argc, const char * argv[]) {
+    
+    int** matrix = new int* [M];
+    
+    for (int i = 0; i < M; i++) {
+        matrix[i] = new int [N];
+    }
+    
+    
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            matrix[i][j] = arc4random() % 2 ? (arc4random() % 100) : -(arc4random() % 100);
+        }
+    }
+    
+    
+    cout << sum_rows(matrix, M, N) << endl;
+    
+    return 0;
+}
