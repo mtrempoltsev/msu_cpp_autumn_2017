5d9e74d03f1c5cf8a82784a25998e0a941c34c87
ekaterina <ekatlmsh@yandex.ru>
2017-09-26 16:55:07 +0300

    clang-format 01 kuznetsova

--- homework/Kuznetsova/01/sum_by_rows.cpp
+++ homework/Kuznetsova/01/sum_by_rows.cpp
@@ -1,58 +1,52 @@
-#include <iostream>
 #include <chrono>
 #include <iostream>
 
-using namespace std;
+class Timer {
+ public:
+  Timer() : start_(std::chrono::high_resolution_clock::now()) {}
 
-class Timer
-{
-public:
-    Timer()
-        : start_(std::chrono::high_resolution_clock::now())
-    {
-    }
+  ~Timer() {
+    const auto finish = std::chrono::high_resolution_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::microseconds>(finish -
+                                                                       start_)
+                     .count()
+              << " us (microseconds)" << std::endl;
+  }
 
-    ~Timer()
-    {
-        const auto finish = std::chrono::high_resolution_clock::now();
-        std::cout << std::chrono::duration_cast<std::chrono::microseconds>(finish - start_).count() << " us (microseconds)" << std::endl;
-    }
-
-private:
-    const std::chrono::high_resolution_clock::time_point start_;
+ private:
+  const std::chrono::high_resolution_clock::time_point start_;
 };
 
 int main() {
+  const int DIM = 10000;
 
-    const int DIM = 10000;
+  int i = 0;
+  int j = 0;
+  volatile int sum = 0;
 
-    int i = 0;
-    int j = 0;
-    volatile int sum = 0;
+  int** matrix = new int*[DIM];
+  for (i = 0; i < DIM; i++) {
+    matrix[i] = new int[DIM];
+  }
 
-    int** matrix = new int*[DIM];
-    for(i = 0; i < DIM; i++) {
-        matrix[i] = new int[DIM];
+  for (i = 0; i < DIM; i++) {
+    for (j = 0; j < DIM; j++) {
+      matrix[i][j] = 1;
     }
+  }
 
-    for(i = 0; i < DIM; i++) {
-        for(j = 0; j < DIM; j++) {
-            matrix[i][j] = 1;
-        }
+  Timer t;
+
+  for (i = 0; i < DIM; i++) {
+    for (j = 0; j < DIM; j++) {
+      sum += matrix[i][j];
     }
+  }
 
-    Timer t;
+  for (i = 0; i < DIM; i++) {
+    delete[] matrix[i];
+  }
+  delete[] matrix;
 
-    for(i = 0; i < DIM; i++) {
-        for(j = 0; j < DIM; j++) {
-            sum += matrix[i][j];
-        }
-    }
-
-    for(i = 0 ; i < DIM; i++) {
-        delete [] matrix[i] ;
-    }
-    delete [] matrix ;
-
-    return 0;
+  return 0;
 }
