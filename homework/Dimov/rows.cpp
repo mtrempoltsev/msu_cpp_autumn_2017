9c1d7536544446447ff71d80e01afc414f161abe
Iliya Dimov <hawkeoni@Iliyas-MacBook-Pro.local>
2017-09-27 21:47:23 +0300

    Added Dimov Ilya's solution to HW1

---
+++ homework/Dimov/rows.cpp
@@ -0,0 +1,16 @@
+#define SIZE 10000
+double a[SIZE][SIZE];
+int i;
+
+int main( int argc, char **argv )
+{
+  int j;
+  int s = 0;
+  for( j = 0; j < SIZE; j++ )
+  {
+    for( i = 0; i < SIZE; i++ )
+    {
+      s += a[j][i];
+    }
+  }
+}
