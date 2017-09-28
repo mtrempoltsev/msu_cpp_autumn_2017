38d24ddf6bcb4d71fc45333fca264fcbd2e83164
vslutov <vslutov@yandex.ru>
2017-09-28 19:16:59 +0300

    Remove ssize_t

--- homework/Lutov/01/benchmark.hpp
+++ homework/Lutov/01/benchmark.hpp
@@ -11,12 +11,12 @@
 
 template<typename value_type, size_t rows, size_t cols>
 time_t
-benchmark(const std::string &sum_type, ssize_t count, const matrix<value_type, rows, cols> &pm, sum_function<value_type, rows, cols> f)
+benchmark(const std::string &sum_type, size_t count, const matrix<value_type, rows, cols> &pm, sum_function<value_type, rows, cols> f)
 {
   std::cout << sum_type << std::endl;
 
   auto t = Timer(count);
-  for (ssize_t i = 0; i < count; ++i) {
+  for (size_t i = 0; i < count; ++i) {
     f(pm);
   }
 
