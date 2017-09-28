c64dc8503578d4b33a941a432423d1fe6f3db078
Evgeniy <cdicec@gmail.com>
2017-09-28 08:53:40 +0300

    my prog

---
+++ homework/pavlov/01/main_cols.cpp
@@ -0,0 +1,44 @@
+#include <chrono>
+#include <iostream>
+
+class Timer
+{
+public:
+    Timer()
+        : start_(std::chrono::high_resolution_clock::now())
+    {
+    }
+
+    ~Timer()
+    {
+        const auto finish = std::chrono::high_resolution_clock::now();
+        std::cout << std::chrono::duration_cast<std::chrono::microseconds>(finish - start_).count() << " us" << std::endl;
+    }
+
+private:
+    const std::chrono::high_resolution_clock::time_point start_;
+};
+
+
+
+int main()
+{
+    volatile static int a[10000][10000];
+    volatile long int sum = 0;
+
+    for(int i = 0; i < 10000; ++i) {
+        for(int j = 0; j < 10000; ++j) {
+            a[i][j] = 1;
+        }
+    }
+	std::cout<<"rows 10000:";
+    auto t = new Timer();
+    for(int i = 0; i < 10000; ++i) {
+        for(int j = 0; j < 10000; ++j) {
+            sum += a[j][i];
+        }
+    }
+    delete t;
+
+    return 0;
+}
