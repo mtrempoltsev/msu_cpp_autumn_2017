b86bc4eb513b07f97034fd23803e9f6f402ff5e0
Fedor <fedorlebed.cs@gmail.com>
2017-10-02 15:55:59 +0300

    removed classes, added tests and comments

--- homework/lebed_fedor/02/parsing_error.cpp
+++ homework/lebed_fedor/02/parsing_error.cpp
@@ -1,40 +1,40 @@
 #include <iostream>
-#include "parsing_error.h"
 
-enum class Color {
+// terminal constants setup font color
+enum class Color
+{
     Red = 35,
     Green = 32
 };
 
-static const char *token_type_str(Token::Type tp) {
-    static const char *t2n[] = { "operation", "number", "EOF" };
-    return t2n[tp];
-}
-
-static void change_color(Color c) {
+// terminal control sequence to setup font color
+void change_color(Color c) {
     std::cout << "\033[" << int(c)   << "m";
 }
 
-static void reset_color() {
+// terminal control sequence to reset font color to normal
+void reset_color() {
     std::cout << "\033[0m";
 }
 
-static void print_mark(int idx) {
+// prints mark symbol to highlight error
+void print_mark(int idx) {
     while (idx--) {
         std::cout << ' ';
     }
     std::cout << '^' << std::endl;
 }
 
-void print_parsing_error(ParseError err, const char *str) {
+// prints parsing error and diagnostic information
+void print_parsing_error(const char *str, const char *mark, const char *error) {
     change_color(Color::Red);
     std::cout << "Erorr: ";
     reset_color();
 
-    std::cout << token_type_str(err.first) << " expected" << std::endl;
+    std::cout << error << std::endl;
     std::cout << str << std::endl;
 
     change_color(Color::Green);
-    print_mark(err.second-str);
+    print_mark(mark-str);
     reset_color();
 }
--- homework/lebed_fedor/02/parsing_error.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "parsing.h"
-
-using ParseError = std::pair<Token::Type, const char *>;
-
-void print_parsing_error(ParseError err, const char *str);
