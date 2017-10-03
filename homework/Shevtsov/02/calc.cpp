c465e3c44f0f9026b0ee4e4717234fe98de7b576
Shevtsov Anton <shevan05@gmail.com>
2017-10-03 22:03:10 +0300

    fixed tabulation hw2

--- homework/Shevtsov/02/calc.cpp
+++ homework/Shevtsov/02/calc.cpp
@@ -7,13 +7,13 @@ using namespace std;
 
 enum class Token
 {
-	Invalid,
-	Minus,
-	Plus,
-	Mul,
-	Div,
-	Number,
-	End
+  Invalid,
+  Minus,
+  Plus,
+  Mul,
+  Div,
+  Number,
+  End
 };
 
 
@@ -22,33 +22,33 @@ int error = 0;
 // считывает токен, меняет позицию прочитанного текста и записывает число в глобальную переменную
 Token getToken(const char*& text)
 {
-	while (const auto c = *text++)
-	{
-		switch (c)
-		{
-			case ' ': continue;
-			case '-': return Token::Minus;
-			case '+': return Token::Plus;
-			case '*': return Token::Mul;
-			case '/': return Token::Div;
-		}
-		if (c >= '0' && c < '9')
-		{
-			--text;
-			auto f = *text;
-			int num = 0;
-			while (f >= '0' && f <= '9') // преобразование многозначного чила в целый тип
-			{
-				num = num * 10 + f - '0';
-				++text;
-				f = *text;
-			}
-			global_num = num;
-			return Token::Number;
-		}
-		return Token::Invalid;
-	}
-	return Token::End;
+  while (const auto c = *text++)
+  {
+    switch (c)
+    {
+      case ' ': continue;
+      case '-': return Token::Minus;
+      case '+': return Token::Plus;
+      case '*': return Token::Mul;
+      case '/': return Token::Div;
+    }
+    if (c >= '0' && c < '9')
+    {
+      --text;
+      auto f = *text;
+      int num = 0;
+      while (f >= '0' && f <= '9') // преобразование многозначного чила в целый тип
+      {
+        num = num * 10 + f - '0';
+	++text;
+	f = *text;
+      }
+      global_num = num;
+      return Token::Number;
+    }
+    return Token::Invalid;
+  }
+  return Token::End;
 }
 //        логика вычислений:
 // <выражение> = <слагаемое> {+ <слагаемое>}  ||
@@ -61,40 +61,40 @@ int primary (Token& token, const char*& text);
 
 int expression (Token& token, const char*& text) { // выражение
   int left = term(token, text);
-	//cout << "expression error" << error << endl;
-	if (error == 1)
-	{
-		return left;
-	}
-  while (token != Token::End) {
-    switch(token)
+    //cout << "expression error" << error << endl;
+    if (error == 1)
     {
-      case Token::Plus:{
-        left += term(token, text);
-				if (error == 1)
-				{
-					return left;
-				}
+      return left;
+    }
+    while (token != Token::End) {
+      switch(token)
+      {
+        case Token::Plus:{
+          left += term(token, text);
+	  if (error == 1)
+	  {
+	    return left;
+	  }
         break;
       }
       case Token::Minus:{
         left -= term(token, text);
-				if (error == 1)
-				{
-					return left;
-				}
+        if (error == 1)
+	{
+	  return left;
+	}
         break;
       }
       case Token::End:{
         return left;
       }
-			default:
-				//cout << "Invalid symbole" << endl;
-				error = 1;
-				return left;
-    }
-	}
+      default:
+	//cout << "Invalid symbole" << endl;
+	error = 1;
 	return left;
+      }
+    }
+    return left;
 }
 
 int term (Token& token, const char*& text) { // слагаемое
@@ -102,8 +102,8 @@ int term (Token& token, const char*& text) { // слагаемое
 	//cout << "term error:" << error << endl;
 	if (error == 1)
 	{
-		//cout << "if-cycle" << endl;
-		return left_term;
+	  //cout << "if-cycle" << endl;
+          return left_term;
 	}
   while (token != Token::End){
     switch(token){
@@ -112,33 +112,33 @@ int term (Token& token, const char*& text) { // слагаемое
         break;
       }
       case Token::Div:{
-				int denominator = primary(token, text);
-					if (denominator == 0) {  // Обработка случая деления на ноль
-						cout << "Error! Can't devide by zero" << endl;
-						error = 1;
-						break;
-					}
-					else {
-        		left_term /= denominator;
-        		break;
-					}
-      }
-			case Token::Plus:{
-				return left_term;
-			}
-			case Token::Minus:{
-				return left_term;
-			}
-			case Token::End:{
-				return left_term;
-			}
-			default:
-				cout << "Invalid symbol" << endl;
-				error = 1;
-				return left_term;
-    }
+	int denominator = primary(token, text);
+	if (denominator == 0) {  // Обработка случая деления на ноль
+	  cout << "Error! Can't devide by zero" << endl;
+	  error = 1;
+          break;
 	}
+	else {
+          left_term /= denominator;
+          break;
+	}
+      }
+      case Token::Plus:{
 	return left_term;
+      }
+      case Token::Minus:{
+	return left_term;
+      }
+      case Token::End:{
+	return left_term;
+      }
+      default:
+        cout << "Invalid symbol" << endl;
+	error = 1;
+	return left_term;
+     }
+  } 
+  return left_term;
 }
 
 int primary (Token& token, const char*& text) { // множитель
@@ -146,26 +146,26 @@ int primary (Token& token, const char*& text) { // множитель
 	//cout << int(token) << endl;
   switch(token){
     case Token::Number: {
-			int v = global_num;
-			token = getToken(text);
+      int v = global_num;
+      token = getToken(text);
     return v;
     }
-		case Token::Minus:{ // унарный минус
-			return -primary(token, text);
-		}
-		/*
-		case Token::End:{
-			cout << "End";
-			int v = global_num;
-			return v;
-		}
-		*/
-		default:
-			cout << "Invalid symbol" << endl;
-			error = 1;
-			int v = global_num;
-			return v;
-  }
+    case Token::Minus:{ // унарный минус
+      return -primary(token, text);
+    }
+    /*
+    case Token::End:{
+      cout << "End";
+      int v = global_num;
+      return v;
+    }
+    */
+    default:
+      cout << "Invalid symbol" << endl;
+      error = 1;
+      int v = global_num;
+      return v;
+    }
 }
 
 
@@ -175,16 +175,15 @@ int primary (Token& token, const char*& text) { // множитель
 
 int main(int argc, char* argv[])
 {
-	//const char* text = "1 * 6 + 3 / 2 - 2 * 2";
-	const char* text = argv[1];
-	//cout << text << endl;
-	Token token; // преобразовали текст в токены
+  //const char* text = "1 * 6 + 3 / 2 - 2 * 2";
+  const char* text = argv[1];
+  //cout << text << endl;
+  Token token; // преобразовали текст в токены
   int result = expression(token, text); // вычислелние выражения
-	if (error == 1)
-	{
-		return 1;
-	}
+  if (error == 1)
+  {
+    return 1;
+  }
   cout << result << endl;
-
-	return 0;
+  return 0;
 }
