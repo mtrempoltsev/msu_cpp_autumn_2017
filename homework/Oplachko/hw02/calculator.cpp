20f8dc0ea2dce17e370938e658bc1bf36bf5a7f8
Nikolay Oplachko <magnickolas@gmail.com>
2017-10-04 17:45:46 +0300

    Fixed hw02

--- homework/Oplachko/hw02/calculator.cpp
+++ homework/Oplachko/hw02/calculator.cpp
@@ -1,37 +1,62 @@
 #include <string>
+#include <cctype>
+#include <cstdlib>
 
-using std::string;
-using str_it = string::const_iterator;
+/*
+* Exit codes
+* 1: incorrect expression notation
+* 2: division by zero
+*/
+
+using str_it = std::string::const_iterator;
+
+int safe_division(int a, int b) {
+	if (b == 0) {
+		exit(2);
+	}
+	return a / b;
+}
 
 enum class Operation {
 	add,
 	sub,
 	mul,
 	div,
-	notOp
+	notOp,
+	endOfExpr
 };
 
-void toNextToken(str_it &it, str_it end) {
-	if (it + 1 == end) {
-		it = end;
-	} else {
-		it += 2;
+str_it skipSpaces(str_it it, str_it end) {
+	if (it == end) {
+		return end;
 	}
+	while (std::isspace(*it)) {
+		it++;
+	}
+	return it;
 }
 
-void toPreviousToken(str_it &it, str_it end) {
-	if (it != end) {
-		it -= 2;
+str_it nextTokenPos(str_it it, str_it end) {
+	if (it == end) {
+		return end;
 	}
+	return skipSpaces(++it, end);
 }
 
-bool isEndTokenPos(str_it it, str_it end) {
-	return it == end || *(it + 1) == ' ';
+str_it prevTokenPos(str_it it, str_it end) {
+	if (it == end) {
+		return end;
+	}
+	it--;
+	while (std::isspace(*it)) {
+		it--;
+	}
+	return it;
 }
 
 Operation getOpType(str_it it, str_it end) {
-	if (!isEndTokenPos(it, end)) {
-		return Operation::notOp;
+	if (it == end) {
+		return Operation::endOfExpr;
 	}
 	switch (*it) {
 	case '+':
@@ -47,7 +72,10 @@ Operation getOpType(str_it it, str_it end) {
 	}
 }
 
-int getNumberSign(str_it &it) {
+int getNumberSign(str_it &it, str_it end) {
+	if (it == end) {
+		exit(1);
+	}
 	if (*it == '-') {
 		it++;
 		return -1;
@@ -56,6 +84,9 @@ int getNumberSign(str_it &it) {
 }
 
 int getNumber(str_it &it, str_it end) {
+	if (it == end || !std::isdigit(*it)) {
+		exit(1);
+	}
 	int res = *it - '0';
 	while (it + 1 != end && isdigit(*(it + 1))) {
 		res = res * 10 + (*++it - '0');
@@ -64,50 +95,56 @@ int getNumber(str_it &it, str_it end) {
 }
 
 int getPrim(str_it &it, str_it end) {
-	int sign = getNumberSign(it);
+	int sign = getNumberSign(it, end);
 	return sign * getNumber(it, end);
 }
 
 int getTerm(str_it &it, str_it end) {
 	int res = getPrim(it, end);
-	toNextToken(it, end);
+	it = nextTokenPos(it, end);
 	while (it != end) {
 		Operation op = getOpType(it, end);
 		if (op != Operation::mul && op != Operation::div) {
 			break;
 		}
-		toNextToken(it, end);
+		it = nextTokenPos(it, end);
 		if (op == Operation::mul) {
 			res *= getPrim(it, end);
 		} else {
-			res /= getPrim(it, end);
+			res = safe_division(res, getPrim(it, end));
 		}
-		toNextToken(it, end);
+		it = nextTokenPos(it, end);
 	}
-	toPreviousToken(it, end);
+	it = prevTokenPos(it, end);
 	return res;
 }
 
-int calcExpr(const string &s) {
+int calcExpr(const std::string &s) {
 	str_it it = s.begin();
 	str_it end = s.end();
 	if (it == end) {
-		return 0;
+		exit(1);
 	}
+	it = skipSpaces(it, end);
 	int res = getTerm(it, end);
-	toNextToken(it, end);
+	it = nextTokenPos(it, end);
 	while (it != end) {
 		Operation op = getOpType(it, end);
 		if (op == Operation::notOp) {
+			exit(1);
+		}
+		if (op == Operation::endOfExpr) {
 			break;
 		}
-		toNextToken(it, end);
+		it = nextTokenPos(it, end);
 		if (op == Operation::add) {
 			res += getTerm(it, end);
-		} else {
+		} else if (op == Operation::sub) {
 			res -= getTerm(it, end);
+		} else {
+			exit(1);
 		}
-		toNextToken(it, end);
+		it = nextTokenPos(it, end);
 	}
 	return res;
 }
