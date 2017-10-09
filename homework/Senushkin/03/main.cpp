8f424bc401fe9d461608d33b77351d8f2470f9a4
Dmitry Senushkin <dmitry.senushkin.msu@gmail.com>
2017-10-09 14:35:38 +0300

    hw03

---
+++ homework/Senushkin/03/main.cpp
@@ -0,0 +1,14 @@
+#include "RDParser.h"
+
+
+int main(int argc, char * argv[]){
+	RDParser& parser = RDParser::Instance();
+	try{
+		std::cout << parser.calculateExprFromStr(argv[1]) << std::endl;
+	}
+	catch(const std::invalid_argument& e){
+		std::cout << e.what() << std::endl;
+	}
+
+	return 0;
+}
