ff6c7dcbf98cd42550d7b4c4f4b7e7b089ca4353
dkoshman <dkoshman@nes.ru>
2017-10-09 09:26:58 +0300

    Koshman_hw03

--- homework/Koshman/01/sum_by_rows.cpp
+++ homework/Koshman/01/sum_by_rows.cpp
@@ -39,7 +39,7 @@ for(int i = 0; i < size; ++i) {
     Timer T;
     for(int i = 0; i < size; ++i) {
         for(int j = 0; j < size; ++j)
-            sum +=array[i][j];
+            sum += array[i][j];
     }
 }
 
