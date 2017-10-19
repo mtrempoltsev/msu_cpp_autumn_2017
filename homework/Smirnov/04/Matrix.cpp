bfa6eb6b7f4f9ceefb71eb022ab2253f0bcafb4d
Fersol <alex-2011.s@yandex.ru>
2017-10-19 12:46:27 +0400

    msu_work_04

---
+++ homework/Smirnov/04/Matrix.cpp
@@ -0,0 +1,113 @@
+#include "Matrix.h"
+
+Matrix::Matrix(size_t rows, size_t cols)
+{	
+	n_rows = rows;
+	n_cols = cols;
+	A = new double* [n_rows];
+    for(size_t i = 0; i < n_rows; i++)
+    {
+        A[i] = new double [n_cols];
+    }
+}
+
+Matrix::~Matrix()
+{	
+	
+	for(size_t i = 0; i < n_rows; i++)
+    {	
+        delete[] A[i];
+    }
+    delete[] A;
+    
+}
+
+size_t Matrix::get_n_rows()
+{
+	return n_rows;
+}
+
+size_t Matrix::get_n_cols()
+{
+	return n_cols;
+}
+
+Matrixrow Matrix::operator[](size_t row)
+{	
+	assert(row < n_rows && row >= 0);
+	return Matrixrow(A[row], n_cols);
+}
+
+Matrix& Matrix::operator*(double value)
+{	
+	for(size_t i = 0; i < n_rows; i++)
+	{
+		for(size_t j = 0; j < n_cols; j++)
+		{
+			A[i][j]*=value;
+		}
+	}
+	return *this;
+}
+
+bool Matrix::operator==(Matrix& B) 
+{	
+	if (n_rows != B.get_n_rows()) return false;
+	if (n_cols != B.get_n_cols()) return false;
+
+    for (size_t i = 0; i < n_rows; ++i)
+    {
+    	for (size_t j = 0; j < n_cols; ++j)
+    	{
+    		if (A[i][j] != B[i][j]) return false;
+    	}
+    }
+                
+    return true;
+}
+
+bool Matrix::operator!=(Matrix& B) 
+{	
+	if (n_rows != B.get_n_rows()) return true;
+	if (n_cols != B.get_n_cols()) return true;
+
+    for (size_t i = 0; i < n_rows; ++i)
+    {
+    	for (size_t j = 0; j < n_cols; ++j)
+    	{
+    		if (A[i][j] != B[i][j]) return true;
+    	}
+    }
+                
+    return false;
+}
+
+vector<double> Matrix::operator*(vector<double> v)
+{
+	assert(v.size() == n_cols);
+	vector<double> res(n_rows);
+
+
+	for(size_t i = 0; i < n_rows; i++)
+	{	
+		res[i] = 0;
+		for(size_t j = 0; j < n_cols; j++)
+		{
+			res[i] += A[i][j] * v[j];
+		}
+	}
+
+	return res;
+}
+
+Matrixrow::Matrixrow(double* row, size_t col_size)
+{
+	Arow = row;
+	size = col_size;
+}
+
+double& Matrixrow::operator[](size_t col)
+{	
+	assert(col < size && col >= 0);
+	return Arow[col];
+}
---
+++ homework/Smirnov/04/test.cpp
@@ -0,0 +1,239 @@
+#include "Matrix.h"
+#include <iostream>
+
+bool test_index()
+{
+	Matrix m(2, 3);
+	m[0][0] = 1;
+    m[0][1] = 2;
+    m[0][2] = 3;
+    m[1][0] = 4;
+    m[1][1] = 5;
+    m[1][2] = 6;
+
+    if (m[0][0] == 1 && m[0][1] == 2 &&  m[0][2] == 3 
+    		&& m[1][0] == 4 && m[1][1] == 5 && m[1][2] == 6)
+    {
+    	return true;
+    }
+
+    return false;
+}
+
+
+bool test_num_cols()
+{
+	Matrix m(2, 3);
+	m[0][0] = 1;
+    m[0][1] = 2;
+    m[0][2] = 3;
+    m[1][0] = 4;
+    m[1][1] = 5;
+    m[1][2] = 6;
+
+    if (m.get_n_cols() == 3)
+    {
+    	return true;
+    }
+
+    return false;
+
+}
+
+bool test_num_rows()
+{
+	Matrix m(2, 3);
+	m[0][0] = 1;
+    m[0][1] = 2;
+    m[0][2] = 3;
+    m[1][0] = 4;
+    m[1][1] = 5;
+    m[1][2] = 6;
+
+    if (m.get_n_rows() == 2)
+    {
+    	return true;
+    }
+
+    return false;
+
+}
+
+bool test_equal()
+{
+	Matrix m(2, 3);
+	m[0][0] = 1;
+    m[0][1] = 2;
+    m[0][2] = 3;
+    m[1][0] = 4;
+    m[1][1] = 5;
+    m[1][2] = 6;
+
+    Matrix k(2, 3);
+	k[0][0] = 1;
+    k[0][1] = 2;
+    k[0][2] = 3;
+    k[1][0] = 4;
+    k[1][1] = 5;
+    k[1][2] = 6;
+
+    if ((k==m) == true)
+    {
+    	return true;
+    }
+
+    return false;
+}
+
+bool test_not_equal()
+{
+	Matrix m(2, 3);
+	m[0][0] = 1;
+    m[0][1] = 2;
+    m[0][2] = 3;
+    m[1][0] = 4;
+    m[1][1] = 5;
+    m[1][2] = 6;
+
+    Matrix k(2, 3);
+	k[0][0] = 4;
+    k[0][1] = 5;
+    k[0][2] = 6;
+    k[1][0] = 1;
+    k[1][1] = 2;
+    k[1][2] = 3;
+
+    if ((k!=m) == true)
+    {
+    	return true;
+    }
+
+    return false;
+}
+
+bool test_mul_value()
+{
+	Matrix m(2, 3);
+	m[0][0] = 1;
+    m[0][1] = 2;
+    m[0][2] = 3;
+    m[1][0] = 4;
+    m[1][1] = 5;
+    m[1][2] = 6;
+
+    double value = 2;
+
+    m = m*value;
+
+    if (m[0][0] == 2 && m[0][1] == 4 &&  m[0][2] == 6 
+    		&& m[1][0] == 8 && m[1][1] == 10 && m[1][2] == 12)
+    {
+    	return true;
+    }
+
+    return false;
+}
+
+bool test_mul_vector()
+{
+	Matrix m(2, 3);
+	m[0][0] = 1;
+    m[0][1] = 2;
+    m[0][2] = 3;
+    m[1][0] = 4;
+    m[1][1] = 5;
+    m[1][2] = 6;
+
+    vector<double> v={1,2,3};
+
+    vector<double> res = m*v;
+
+    if (res[0] == 14 && res[1] == 32)
+    {
+    	return true;
+    }
+
+    return false;
+}
+
+
+void Test()
+{	
+
+	std::cout << "Index test: ";
+	if (test_index()) 
+	{
+		std::cout << "pass" << endl;
+	}
+	else
+	{
+		std::cout << "not pass" << endl;
+	}
+
+	std::cout << "Columns test: ";
+	if (test_num_cols()) 
+	{
+		std::cout << "pass" << endl;
+	}
+	else
+	{
+		std::cout << "not pass" << endl;
+	}
+
+	std::cout << "Rows test: ";
+	if (test_num_rows()) 
+	{
+		std::cout << "pass" << endl;
+	}
+	else
+	{
+		std::cout << "not pass" << endl;
+	}
+
+	std::cout << "Equal test: ";
+	if (test_equal()) 
+	{
+		std::cout << "pass" << endl;
+	}
+	else
+	{
+		std::cout << "not pass" << endl;
+	}
+
+	std::cout << "Not equal test: ";
+	if (test_not_equal()) 
+	{
+		std::cout << "pass" << endl;
+	}
+	else
+	{
+		std::cout << "not pass" << endl;
+	}
+
+	std::cout << "Multiply double test: ";
+	if (test_mul_value()) 
+	{
+		std::cout << "pass" << endl;
+	}
+	else
+	{
+		std::cout << "not pass" << endl;
+	}
+
+	std::cout << "Multiply vector test: ";
+	if (test_mul_vector()) 
+	{
+		std::cout << "pass" << endl;
+	}
+	else
+	{
+		std::cout << "not pass" << endl;
+	}
+}
+
+
+int main(int argc, char** argv)
+{	
+	Test();
+	return 0;
+}
