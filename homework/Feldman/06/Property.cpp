07ae37e1135fbd38501b8775be6d92282f121ee4
borisman18 <32166232+borisman18@users.noreply.github.com>
2017-11-09 19:36:31 +0300

    Delete Property.cpp

--- homework/Feldman/06/Property.cpp
+++
@@ -1,9 +0,0 @@
-#include <limits>
-
-template <typename T>
-struct NumericTraits
-{
-	static constexpr T min = std::numeric_limits<T>::lowest();
-	static constexpr T max = std::numeric_limits<T>::max();
-};
-
