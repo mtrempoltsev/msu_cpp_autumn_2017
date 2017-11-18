c90c8d900db8ce5dacf650086e1166ff9b4005fa
Microflame <spitfireage@gmail.com>
2017-11-18 18:16:58 +0300

    hw 09 fix

--- homework/Дорожинский/HW09/main.cpp
+++ homework/Дорожинский/HW09/main.cpp
@@ -1,50 +1,52 @@
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <vector>
 
 class Counter {
 public:
-	Counter& read(char* const fname) {
-		std::string word;
-		std::ifstream file(fname);
-		check_fstream_open(file);
-		
-		file >> word;
-		while (file.good()) {
-		    ++freqs.emplace(word, 0).first->second;
-		    file >> word;
-		}
-		return *this;
-	}
+    Counter& read(char* const fname) {
+        std::string word;
+        std::ifstream file(fname);
+        check_fstream_open(file);
+        
+        while (file >> word) {
+            ++freqs.emplace(word, 0).first->second;
+        }
+        return *this;
+    }
 
-	Counter& write(char* const fname) {
-		std::ofstream file(fname);
-		check_fstream_open(file);
-	
-		for (auto it = freqs.rbegin(); it != freqs.rend(); ++it) { // std::map is ordered
-			file << it->first << " " << it->second << std::endl;
-		}
-		return *this;
-	}
+    Counter& write(char* const fname) {
+        using pair = std::pair<std::string, int>;
+        std::ofstream file(fname);
+        check_fstream_open(file);
+
+        std::vector<pair> freqs_sorted(freqs.begin(), freqs.end());
+        std::sort(freqs_sorted.begin(), freqs_sorted.end(), [](pair a, pair b){ return a.second > b.second; });
+    
+        for (auto it = freqs_sorted.begin(); it != freqs_sorted.end(); ++it) {
+            file << it->first << " " << it->second << std::endl;
+        }
+        return *this;
+    }
 
 private:
-	void check_fstream_open(const std::ios& fstream) {
-		if (!fstream) {
-		    std::cout << "Can't open file.";
-		    exit(1);
-		}
-	}
+    void check_fstream_open(const std::ios& fstream) {
+        if (!fstream) {
+            std::cout << "Can't open file.";
+            exit(1);
+        }
+    }
 
-	std::map<std::string, int> freqs;
+    std::map<std::string, int> freqs;
 };
 
 
 int main(int argc, char* const argv[]) {
-	if (argc != 3) {
-		std::cout << "Invalid arguments count" << std::endl;
-		exit(1);
-	}
-	Counter c;
-	c.read(argv[1]).write(argv[2]);
-	return 0;
+    if (argc != 3) {
+        std::cout << "Invalid arguments count" << std::endl;
+        exit(1);
+    }
+    Counter().read(argv[1]).write(argv[2]);
+    return 0;
 }
